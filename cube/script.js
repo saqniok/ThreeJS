586d3ff6804e67c4038888d5c69667572d90004b
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-05-23 16:46:52 +0200

    threejs cubes

--- cube/script.js
+++ cube/script.js
@@ -5,11 +5,44 @@ import * as THREE from 'three';
 const scene = new THREE.Scene()
 console.log(scene)
 
-// Red CUBDE
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 'blue'});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// CUBES
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshBasicMaterial({color: 'red'})
+)
+cube1.position.set(0, 1, 0)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshBasicMaterial({color: 'blue'})
+)
+cube2.position.set(1, 0, 0)
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshBasicMaterial({color: 'yellow'})
+)
+cube3.position.set(0, 0, 1.5)
+group.add(cube3)
+
+// Posistion
+
+
+// Scale
+
+// Rotation
+
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+
 
 // Sizes
 const sizes = {
@@ -19,8 +52,9 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.set(1, 1, 3)
+camera.position.set(1, 1, 4)
 scene.add(camera);
+
 
 // Renderer
 const canvas = document.querySelector('.webgl');

b39b646dc203ae6877029645fcc5b9dac5117ed6
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-05-24 16:31:49 +0200

    cube

--- cube/script.js
+++ cube/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 
 // Scene
@@ -11,27 +12,26 @@ scene.add(group)
 
 const cube1 = new THREE.Mesh(
     new THREE.BoxGeometry(0.5, 0.5, 0.5),
-    new THREE.MeshBasicMaterial({color: 'red'})
+    new THREE.MeshBasicMaterial({color: 'yellow'})
 )
 cube1.position.set(0, 1, 0)
 group.add(cube1)
 
 const cube2 = new THREE.Mesh(
     new THREE.BoxGeometry(0.5, 0.5, 0.5),
-    new THREE.MeshBasicMaterial({color: 'blue'})
+    new THREE.MeshBasicMaterial({color: 'red'})
 )
 cube2.position.set(1, 0, 0)
 group.add(cube2)
 
 const cube3 = new THREE.Mesh(
     new THREE.BoxGeometry(0.5, 0.5, 0.5),
-    new THREE.MeshBasicMaterial({color: 'yellow'})
+    new THREE.MeshBasicMaterial({color: 'blue'})
 )
 cube3.position.set(0, 0, 1.5)
 group.add(cube3)
 
 // Posistion
-
 
 // Scale
 
@@ -62,7 +62,45 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+
+// FPS 
+let time = Date.now();
+const clock = new THREE.Clock();
+
+// // GSAP
+// gsap.to(cube1.position, { duration: 1, delay: 1, x: 2});
 
 // Animations
-const tick = () => { console.log(tick);window.requestAnimationFrame(tick) }
+const tick = () => { 
+
+    // Time
+    const currentTime = Date.now();
+    
+    const deltaTime = currentTime - time;
+    time = currentTime;
+
+
+    // Clock
+    const elapsedTime = clock.getElapsedTime();
+
+
+    // Update Objects
+    cube1.rotation.y += 0.001 * deltaTime;
+    cube1.position.x = Math.sin(elapsedTime);
+
+    cube2.rotation.y = elapsedTime;
+    cube2.position.z = Math.sin(elapsedTime);
+
+    cube3.rotation.y += 0.001 * deltaTime;
+    cube3.position.y = Math.cos(elapsedTime);
+
+    camera.lookAt(axesHelper.position);
+
+    // Update Renderer
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick); }
+
+tick();
